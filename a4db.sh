cf9c590fa034d1950ed539b8d8c2505147dd839b
michaelruigrok <ruigrok.michael@gmail.com>
2016-10-21 14:36:02 +1000

    vimdo replace argcount check, a4db.sh valgrind option

--- a4db.sh
+++ a4db.sh
@@ -1,15 +1,18 @@
 if [[ $# -eq 0 ]]; then
     echo "You need a test argument bro";
-    echo "Usage: a4db TESTNAME DIFFCOLUMNS";
+    echo "Usage: a4db TESTNAME OPTION";
     echo "(a4db.sh --help for help)";
     exit;
 fi
 
 if [[ $1 = "--help" ]]; then
-    echo "Usage: a4db TESTNAME DIFFCOLUMNS"
+    echo "Usage: a4db TESTNAME OPTION"
     echo
-    echo "if DIFFCOLUMNS is 'single', diff will print in 1 column"
-    echo "if DIFFCOLUMNS is 'vim', vimdiff will run instead"
+    echo "if OPTION is 'valgrind', program will print valgrind"
+    echo " results into valgrind.out, and then open that file in less"
+    echo
+    echo "if OPTION is 'single', diff will print in 1 column"
+    echo "if OPTION is 'vim', vimdiff will run instead"
     echo "otherwise it will diff in side-by-side mode"
     echo
     echo "This program will:"
@@ -27,36 +30,47 @@ testprogram="testa4.sh explain $1"
 
 #get the executable and arguments for that specific test
 test=`$testprogram | awk 'NR==8'`
-echo $test
-eval $test #run it
+testexec=`echo $test | cut -d\  -f 1`
+testargs=`echo $test | cut -d\  -f 2- | rev | cut -d\  -f 5- | rev`
 
 #take the arguments and construct a gdb run argument out of them (in gdb.run)
 #type 'so[urce] gdb.run' to use it
 echo -n 'r ' > ./gdb.run
-echo "$test" | cut -d\  -f 2- | rev | cut -d\  -f 5- | rev >> ./gdb.run
+echo $testargs >> ./gdb.run
 
-diffarg='diff -y '
-if [[ $2 = 'single' ]]; then
-	diffarg='diff '
-	echo "second argument is 'single'; gonna output diff in one column"
-elif [[ $2 = 'vim' ]]; then
-	diffarg='vimdiff '
-	echo "second argument is 'vim'; gonna output diff as vimdiff"
-elif [[ $# -eq 2 ]]; then
-	echo "second argument can be either 'single' or 'vim', see a4db.sh --help"
+if [[ $2 = 'valgrind' ]]; then
+>&2 echo "the valgrind option is currently broken. You can run this command yourself though:"
+valcommand="valgrind --track-origins=yes $testexec $testargs 2> valgrind.out"
+echo $valcommand
+#cat valgrind.out
+
+else
+    echo $test
+    eval $test #run test
+
+    diffarg='diff -y '
+    if [[ $2 = 'single' ]]; then
+        diffarg='diff '
+        echo "second argument is 'single'; gonna output diff in one column"
+    elif [[ $2 = 'vim' ]]; then
+        diffarg='vimdiff '
+        echo "second argument is 'vim'; gonna output diff as vimdiff"
+    elif [[ $# -eq 2 ]]; then
+        echo "second argument can be either 'single' or 'vim', see a4db.sh --help"
+    fi
+
+    for stream in 'out' 'err';
+    do
+        diffthis=`$testprogram | grep ".$stream tests/" | cut -d\  -f 2-`
+        if [[ -z $diffthis ]]; then continue; fi #continue if empty
+
+        eval "diff -q" $diffthis > /dev/null
+        if [[ $? != 0 ]]; then
+            echo "std$stream:"
+            eval $diffarg $diffthis
+            echo
+        else
+            echo "std$stream matches!"
+        fi
+    done
 fi
-
-for stream in 'out' 'err';
-do
-	diffthis=`$testprogram | grep ".$stream tests/" | cut -d\  -f 2-`
-	if [[ -z $diffthis ]]; then continue; fi #continue if empty
-
-	eval "diff -q" $diffthis > /dev/null
-	if [[ $? != 0 ]]; then
-		echo "std$stream:"
-		eval $diffarg $diffthis
-	   	echo
-	else
-		echo "std$stream matches!"
-	fi
-done
