365d6b72ea3124f5adeca2b3ba46c248efdb4e24
michaelruigrok <ruigrok.michael@gmail.com>
2016-10-25 20:03:51 +1000

    added selective testing, unimplemented attampt to get multi-exec tests working

--- a4db.sh
+++ a4db.sh
@@ -9,6 +9,7 @@ if [[ $# -eq 0 ]]; then
     echo "You need a test argument bro";
     echo "Usage: a4db [TESTNAME [OPTION]]";
     echo "   or: a4db list;"
+    echo "   or: a4db test [team|controller];"
     echo "(a4db.sh --help for help)";
     exit;
 fi
@@ -32,6 +33,7 @@ case $1 in
     echo "   -m, --valgrind         run the test in valgrind & output in less"
     echo
     echo "if argument is 'list', a4db.sh will list all possible tests"
+    echo "if argument is 'test', a4db.sh run all tests for team/controller only"
     echo
     echo "1) Run the associated test"
     echo "2) print both the stdout and stderr diffs"
@@ -49,6 +51,27 @@ list)
     contTests=`$testnames | awk '/class Controller/,0{ if (/def/) {print}}' | sed -r -e 's/.* (.*)\(.*/Controller.\1/'`
     echo "$teamTests" "$contTests"
     #$testprogram | awk 
+    exit
+    ;;
+
+test)
+    testnames="cat tests/grum.py"
+    teamTests=`$testnames | awk '/class Team/,/class Controller/{ if (/def/) {print}}' | sed -r -e 's/.* (.*)\(.*/Team.\1/'`
+    contTests=`$testnames | awk '/class Controller/,0{ if (/def/) {print}}' | sed -r -e 's/.* (.*)\(.*/Controller.\1/'`
+    case $2 in
+        team)
+            testa4.sh test $teamTests
+            ;;
+        con*)
+            testa4.sh test $contTests
+            ;;
+        '')
+            testa4.sh
+            ;;
+        *)
+            testa4.sh test $2
+            ;;
+    esac
     exit
     ;;
 
@@ -75,52 +98,104 @@ list)
 
 esac 
 
+
+
 testprogram="testa4.sh explain $1"
 
 #get the executable and arguments for that specific test
-test=`$testprogram | awk '/.\/2310/'`
+test=`$testprogram | awk '/Start/{getline; print;}'`
 
-if [[ `echo $test | grep \"\" -c` != 0 ]]; then
+if [[ `echo "$test" | wc -l` = 1 ]]; then
+
+    # "" has replaced an option in this mode, cannot run
+    if [[ `echo $test | grep 'ncserv_port|\"\"' -c` != 0 ]]; then
+        $testprogram
+        echo ---------------------------------------------------------------------
+        echo "can't do anything with this, sorry :("
+        exit
+    fi
+
+    testexec=`echo $test | cut -d\  -f 1`
+    testargs=`echo $test | cut -d\  -f 2- | rev | cut -d\  -f 5- | rev`
+
+    #take the arguments and construct a gdb run argument out of them (in gdb.run)
+    #type 'so[urce] gdb.run' to use it
+    echo -n 'r ' > ./gdb.run
+    echo $testargs >> ./gdb.run
+
+    if [[ $# -gt 1 ]]; then
+        echo "Just in case you didn't know, the way this script works has changed."
+        echo "Check out --help to get back on track if need be"
+
+    elif [[ $mode = 'valgrind' ]]; then
+        valcommand="valgrind --log-fd=1 --track-origins=yes $testexec $testargs"
+        echo $valcommand
+        eval $valcommand | less
+
+    else
+        echo $test
+        eval $test #run test
+
+        for stream in 'out' 'err';
+        do
+            diffthis=`$testprogram | grep ".$stream tests/" | cut -d\  -f 2-`
+            if [[ -z $diffthis ]]; then continue; fi #continue if empty
+
+            eval "diff -q" $diffthis > /dev/null
+            if [[ $? != 0 ]]; then
+                echo "std$stream:"
+                eval $diffarg $diffthis
+                echo
+            else
+                echo "std$stream matches!"
+            fi
+        done
+    fi
+
+else
+
+    test=echo "$test" | awk '/.\/2310/{print;exit}'
+    echo $test
+    testexec=`echo $($test) | cut -d\  -f 1`
+    testargs=`echo $($test) | cut -d\  -f 2- | rev | cut -d\  -f 5- | rev`
+
+    #take the arguments and construct a gdb run argument out of them (in gdb.run)
+    #type 'so[urce] gdb.run' to use it
+    echo -n 'r ' > ./gdb.run
+    echo $testargs >> ./gdb.run
+
     $testprogram
-    #$testprogram | awk 'NR==7,/Expect exit status/'
+
     echo ---------------------------------------------------------------------
     echo "can't do anything with this, sorry :("
     exit
+    firsttest="$(echo "$test" | awk 'NR==1')"
+
+    ftestout="$(echo "$firsttest" | grep -E -o '\S*.out')"
+    ftesterr="$(echo "$firsttest" | grep -E -o '\S*.err')"
+
+    rm -f out.a4db err.a4db
+    mkfifo out.a4db err.a4db
+    firsttest="$(echo "$firsttest" | sed -e "s/$ftestout/out.a4db/g" -e "s/$ftesterr/err.a4db/g")"
+    echo $firsttest
+
+    eval $firsttest & echo sdfhaslfjaslfjsadldfsaldjf
+    #$echo "$firsttest" > $ftestout & echo sdfhaslfjaslfjsadldfsaldjf
+    portno="$(awk '{print $1; exit;}' < $ftesterr)"
+    echo Portno:
+    echo $portno
+    othertests="$(echo "$test" | awk '1,0{print;}' | sed -e "s/\"\"\|\[ncserv_port\]/$portno/g")"
+    echo "$othertests"
+    #test=echo "$othertests" | perl -pe 's/\n/ \&'
+    echo "$othertests"
+    sleep 1
+    kill $!
+    
+    cat portno.a4db
+    rm portno.a4db
+    echo tail
+    echo $portno
+    cat $ftestout
+    exit
 fi
 
-testexec=`echo $test | cut -d\  -f 1`
-testargs=`echo $test | cut -d\  -f 2- | rev | cut -d\  -f 5- | rev`
-
-#take the arguments and construct a gdb run argument out of them (in gdb.run)
-#type 'so[urce] gdb.run' to use it
-echo -n 'r ' > ./gdb.run
-echo $testargs >> ./gdb.run
-
-if [[ $# -gt 1 ]]; then
-    echo "Just in case you didn't know, the way this script works has changed."
-    echo "Check out --help to get back on track if need be"
-
-elif [[ $mode = 'valgrind' ]]; then
-    valcommand="valgrind --log-fd=1 --track-origins=yes $testexec $testargs"
-    echo $valcommand
-    eval $valcommand | less
-
-else
-    echo $test
-    eval $test #run test
-
-    for stream in 'out' 'err';
-    do
-        diffthis=`$testprogram | grep ".$stream tests/" | cut -d\  -f 2-`
-        if [[ -z $diffthis ]]; then continue; fi #continue if empty
-
-        eval "diff -q" $diffthis > /dev/null
-        if [[ $? != 0 ]]; then
-            echo "std$stream:"
-            eval $diffarg $diffthis
-            echo
-        else
-            echo "std$stream matches!"
-        fi
-    done
-fi
